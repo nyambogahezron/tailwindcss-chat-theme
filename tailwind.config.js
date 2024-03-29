5c8cc723a40262b2ae90da96502a3d7a6d483d8d
Hezron Nyamboga <hezzyn1@gmail.com>
2024-03-29 15:53:28 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '700px',
+      lg: '976px',
+      xl: '1280px',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
